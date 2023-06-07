1f767fa0e2e4b82892318a028b507d5745212b3f
よっしぃ <69622321+Notyoss1@users.noreply.github.com>
2023-06-07 12:28:49 +0900

    Add files via upload

---
+++ もぅマヂ無理リスカしよ.py
@@ -0,0 +1,9 @@
+﻿import time
+
+waitsec = float(input("回答後何秒待ちますか?"))
+answer = str(input("もぅマヂ無理?(y/n)"))
+if answer == "y":
+    print("もぅマヂ無理リスカしよ")
+
+print(waitsec,"秒後に終了します...")
+time.sleep(waitsec)
